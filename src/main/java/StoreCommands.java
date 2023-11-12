c9c07bf908d99e697bbd0b715a7fab8bc2cd6ba3
Benna <br599@drexel.edu>
2023-11-12 17:05:34 -0500

    "Command storage class and its test file are finished"

---
+++ src/main/java/StoreCommands.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StoreCommands {
+    private List<String> invalidCommands;
+
+    public StoreCommands() {
+        this.invalidCommands = new ArrayList<>();
+    }
+
+    public void addInvalidCommand(String command) {
+        invalidCommands.add(command);
+    }
+
+    public List<String> getAllInvalidCommands() {
+        return new ArrayList<>(invalidCommands);
+    }
+}
---
+++ src/test/java/StoreCommandsTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StoreCommandsTest {
+
+    @Test
+    void store_invalid_command() {
+        StoreCommands storeCommands = new StoreCommands();
+
+        storeCommands.addInvalidCommand("create 12345678 2.0");
+        storeCommands.addInvalidCommand("12345789 100");
+
+        assertEquals(2, storeCommands.getAllInvalidCommands().size());
+        assertEquals("create 12345678 2.0", storeCommands.getAllInvalidCommands().get(0));
+        assertEquals("12345789 100", storeCommands.getAllInvalidCommands().get(1));
+    }
+}
