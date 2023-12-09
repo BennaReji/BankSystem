fda8c78ec47d12d0ccb1857bc28986e4e5d1a0d8
Benna <br599@drexel.edu>
2023-12-09 11:50:25 -0500

    "The pass time validator test cases and pass time validator class are finished"

---
+++ src/main/java/banking/PassTimeValidator.java
@@ -0,0 +1,33 @@
+package banking;
+
+public class PassTimeValidator extends CommandValidator {
+    private static final int MIN_MONTHS = 1;
+    private static final int MAX_MONTHS = 60;
+
+    @Override
+    public boolean validate(String command) {
+        String[] parts = command.split(" ");
+        if (parts.length != 2) {
+            return false;
+        }
+
+        String commandWord = parts[0].toLowerCase();
+        if (!commandWord.equals("pass")) {
+            return false;
+        }
+
+        int months;
+        try {
+            months = Integer.parseInt(parts[1]);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        return validMonthRange(months);
+    }
+
+    private boolean validMonthRange(int months) {
+        return months >= MIN_MONTHS && months <= MAX_MONTHS;
+    }
+}
+
---
+++ src/test/java/banking/PassTimeValidatorTest.java
@@ -0,0 +1,41 @@
+package banking;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PassTimeValidatorTest {
+    PassTimeValidator passTimeValidator;
+
+
+    @BeforeEach
+    void setUp() {
+        passTimeValidator = new PassTimeValidator();
+
+    }
+
+    @Test
+    void valid_pass_time_command() {
+        assertTrue(passTimeValidator.validate("pass 1"));
+        assertTrue(passTimeValidator.validate("pass 12"));
+        assertTrue(passTimeValidator.validate("pass 60"));
+    }
+
+    @Test
+    void invalid_pass_time_command_out_of_range() {
+        assertFalse(passTimeValidator.validate("pass 0"));
+        assertFalse(passTimeValidator.validate("pass 61"));
+        assertFalse(passTimeValidator.validate("pass 100"));
+    }
+
+    @Test
+    void invalid_pass_time_command_with_wrong_format() {
+        assertFalse(passTimeValidator.validate("pas 12"));
+        assertFalse(passTimeValidator.validate("pass th"));
+        assertFalse(passTimeValidator.validate("12"));
+    }
+
+
+}
